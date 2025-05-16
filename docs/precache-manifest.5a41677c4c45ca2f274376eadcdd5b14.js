7b7287c1a59880a7d918a5f34095f4b4800f04c3
kikuchi5555 <kikutetsu.5555+github@gmail.com>
2025-05-16 10:14:19 +0900

    build

--- docs/precache-manifest.5a41677c4c45ca2f274376eadcdd5b14.js
+++ docs/precache-manifest.5a41677c4c45ca2f274376eadcdd5b14.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "82b580a23274d8d74da55070ed985d08",
+    "revision": "72f206877c92e7eb657985aba861144b",
     "url": "/index.html"
   },
   {
-    "revision": "4c8dc842e0faf03a3299",
+    "revision": "ac9fb19723a8feeb394f",
     "url": "/static/css/main.55dc384a.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.c70e6c8d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4c8dc842e0faf03a3299",
-    "url": "/static/js/main.2023da7a.chunk.js"
+    "revision": "ac9fb19723a8feeb394f",
+    "url": "/static/js/main.353cfb89.chunk.js"
   },
   {
     "revision": "73635f645c271e35ff15",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09bcbe358c16e6101f7c70594e67f5d1.js"
+  "/precache-manifest.5a41677c4c45ca2f274376eadcdd5b14.js"
 );
 
 self.addEventListener('message', (event) => {
